eaa16f94cec562f508ecf395eb78e2fa709ee13e
Duc Minh La <dminh.bob.la@gmail.com>
2025-09-21 01:54:04 +1000

    updated with better config and confirmed linear is better now (a bit wrong at accompanying)

--- scripts/traj_embed/full_run_small_linear.sh
+++ scripts/traj_embed/full_run_small_linear.sh
@@ -4,7 +4,7 @@ uv run ananke traj-embed \
 -av "src\data\traj_processed\small_activities_homebound_wd.csv" \
 -pv "src\data\traj_processed\purposes_new.csv" \
 -o "src\output\traj_embed\models\small_linear" \
--e 500 \
+-e 1000 \
 -b 32 \
 --lr 1e-3 \
 --val_ratio 0.2 \
@@ -15,7 +15,7 @@ uv run ananke gval-traj \
 --purposes_csv "src\data\traj_processed\purposes_new.csv" \
 --ckpt "src\output\traj_embed\models\small_linear\ckpt_best.pt" \
 --batch_size 32 \
---num_gen 10000 \
+--num_gen 30000 \
 --gen_prefix "gen" \
 --gen_csv "src\output\traj_embed\inference\small_linear\gen_activities.csv" \
 --val_csv "src\output\traj_embed\inference\small_linear\gen_validation.csv" \
