105506d9aefd18748923913f3cac455cb864695b
Duc Minh La <dminh.bob.la@gmail.com>
2025-09-21 01:25:46 +1000

    minir

--- scripts/traj_embed/full_run_small_linear.sh
+++ scripts/traj_embed/full_run_small_linear.sh
@@ -4,7 +4,7 @@ uv run ananke traj-embed \
 -av "src\data\traj_processed\small_activities_homebound_wd.csv" \
 -pv "src\data\traj_processed\purposes_new.csv" \
 -o "src\output\traj_embed\models\small_linear" \
--e 300 \
+-e 500 \
 -b 32 \
 --lr 1e-3 \
 --val_ratio 0.2 \
