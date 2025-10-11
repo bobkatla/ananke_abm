f477d3a8d66fa82b881650bc759307d1e9d008cc
Duc Minh La <dminh.bob.la@gmail.com>
2025-10-12 10:29:51 +1100

    add for m3

---
+++ scripts/m3/run_vae_only_full.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH --job-name=fullMelbAct_linear
+#SBATCH --output=fullMelb_linear.out      # Standard output
+#SBATCH --error=fullMelb_linear.err       # Standard error
+#SBATCH --account=tx89
+#SBATCH --time=90:00:00
+#SBATCH --ntasks=3
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:2
+#SBATCH --partition=gpu
+
+module load miniforge3
+mamba activate ananke
+
+uv run src/ananke_abm/models/traj_syn/pipeline/train_vae_only.py \
+--activities_csv src/data/traj_processed/activities_homebound_wd.csv \
+--purposes_csv src/data/traj_processed/purposes_new.csv \
+--outdir src/output/test_vae \
+--mode recon+marginals --epochs 5000
+
+uv run src/ananke_abm/models/traj_syn/pipeline/synthesize_vae.py \
+--ckpt src/output/test_vae/vae_ep5000.pt \
+--out_csv src/output/test_vae/gen_vae.csv \
+--num_gen 500000
+
+uv run src/ananke_abm/models/traj_syn/eval/analyze_vae.py \
+--gen_csv src/output/test_vae/gen_vae.csv \
+--mean_probs_npy src/output/test_vae/vae_mean_probs_ep5000.npy \
+--purposes "Home,Work,Shopping,Social,Accompanying,Education,Other" \
+--step_minutes 5 \
+--t_alloc_minutes 1800 \
+--out_json src/output/test_vae/vae_diag.json \
+--history_csv src/output/test_vae/vae_history.csv \
+--real_csv src/data/traj_processed/activities_homebound_wd.csv
+
+uv run ananke visualize-combined-traj \
+--traj_csv src/output/test_vae/gen_vae.csv \
+--buffer_csv "src/output/test_vae/buffer_grid.csv" \
+--out_dir "src/output/test_vae" \
+--y_work_max 0.05 \
+--y_edu_max 0.05 \
+--dpi 300
